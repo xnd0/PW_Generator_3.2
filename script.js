8a3c108fb09d8a5a734a910d2efb01c9dab9e56a
xndcmd <xndcmd@gmail.com>
2022-05-31 09:40:19 -0700

    set arrays for lower, upper, numerical, and special characters

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// ----------------------- //
+// ------ Javscript ------ //
+// ----------------------- //
+
+
+
+// Set Arrays
+
+
+var isLower = [ 'a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var isUpper = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var isNumber = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var isChar = [ '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'];
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +36,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+console.log(isLower);
+console.log(isUpper);
+console.log(isNumber);
+console.log(isChar);
+
